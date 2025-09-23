371dbe0639e74b8f99410c930fd2d5c9e6e5a7c0
Domenico Andreoli <domenico.andreoli@elastic.co>
2025-09-23 10:55:26 +0200

    Fix test verbosity configuration

--- test/util.py
+++ test/util.py
@@ -6,7 +6,7 @@ from core.util import setup_logging
 logger = logging.getLogger("elastic.pipes.core")
 set_level = setup_logging()
 
-verbosity = max(arg.count("v") for arg in sys.argv if arg.startswith("-") and not arg.startswith("--"))
+verbosity = sum(arg.count("v") for arg in sys.argv if arg.startswith("-") and not arg.startswith("--"))
 if verbosity > 2:
     set_level(logging.DEBUG)
 elif verbosity > 1:
